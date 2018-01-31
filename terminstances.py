66323382fe29676104c0e1d3f88cbcf4e22a2bd3
hensonengineer <hensonengineer@gmail.com>
2018-01-31 09:09:16 -0700

    Create terminstances.py

---
+++ terminstances.py
@@ -0,0 +1,20 @@
+import boto3
+ec2 = boto3.resource('ec2')
+
+def lambda_handler(event, context):
+    filters = [{
+            'Name': 'your tag name',
+            'Values': ['your tag value']
+        },
+        {
+            'Name': 'instance-state-name', 
+            'Values': ['running']
+        }
+    ]
+    
+    instances = ec2.instances.filter(Filters=filters)
+
+    runningInstances = [instance.id for instance in instances]
+    
+    if len(runningInstances) > 0:
+        shuttingDown = ec2.instances.filter(InstanceIds=runningInstances).terminate()
