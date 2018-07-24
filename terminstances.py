1b5084cfd51ddf047da502fa04bcfccc86159b03
Chris Henson <hensonengineer@gmail.com>
2018-07-24 15:21:14 -0600

    Update terminstances.py
    Added console logging

--- terminstances.py
+++ terminstances.py
@@ -1,10 +1,13 @@
 import boto3
+
+import boto3
 ec2 = boto3.resource('ec2')
 
 def lambda_handler(event, context):
+
     filters = [{
-            'Name': 'your tag name',
-            'Values': ['your tag value']
+            'Name': 'tag:your-tag-name',
+            'Values': ['your-tag-value']
         },
         {
             'Name': 'instance-state-name', 
@@ -14,7 +17,8 @@ def lambda_handler(event, context):
     
     instances = ec2.instances.filter(Filters=filters)
 
-    runningInstances = [instance.id for instance in instances]
-    
-    if len(runningInstances) > 0:
-        shuttingDown = ec2.instances.filter(InstanceIds=runningInstances).terminate()
+
+    for instance in instances:
+        print('Terminating', instance) # log which instances terminate to CloudWatch
+        #instance.terminate() # uncomment when testing complete
+   
